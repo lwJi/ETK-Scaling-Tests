fa00b77dc5484af75e58cca55e3f88da2ac28c73
Liwei Ji <jiliwei.phys@gmail.com>
2024-12-21 10:01:43 -0500

    Utils: add plot_efficiency in misc.jl

--- Utils/misc.jl
+++ Utils/misc.jl
@@ -242,10 +242,7 @@ function plot_scaling(
             plot!(plt, dat[1], dat[2], label = labs[i], marker = mark)
             if is_print_value
                 @printf("  %8s: [", labs[i])
-                for d in dat[2]
-                    @printf(" %8.2e,", d)
-                end
-                @printf("]\n")
+                println(join([@sprintf(" %8.2e", d) for d in dat[2]], ","), "]")
             end
         end
 
@@ -253,6 +250,44 @@ function plot_scaling(
         if (is_plot_ideal)
             x_ref, y_ref = dats[end]  # choose the last dataset as the reference
             ideal_y = y_ref[1] .* (x_ref ./ x_ref[1])  # compute the ideal scaling line
+            plot!(plt, x_ref, ideal_y; label = "Ideal", linestyle = :dash, color = :red)
+        end
+    end
+end
+
+# Plot efficiency
+function plot_efficiency(
+    plt::Plots.Plot,
+    patt_dirss::Vector{Tuple{Vector{Tuple{Regex,String}},String,Symbol}};
+    option::String = "ZcsPerSecond",
+    is_print_value::Bool = false,
+    is_plot_ideal::Bool = false,
+)::Nothing
+    @assert !isempty(patt_dirss) "The `patt_dirss` input cannot be empty."
+
+    # Process datasets
+    for (patterns, parent_dir, mark) in patt_dirss
+        @assert isdir(parent_dir) "Invalid directory: $parent_dir"
+
+        # Load averages for the given patterns and directory
+        dats, labs = load_avgs(patterns, parent_dir; option = option)
+        @assert !isempty(dats) "No data found for directory: $parent_dir"
+
+        # Iterate through the loaded datasets
+        for (i, dat) in enumerate(dats)
+            value_per_node = dat[2] ./ dat[1]  # Zcs/sec/node
+            efficiency = value_per_node ./ first(value_per_node)
+            plot!(plt, dat[1], efficiency, label = labs[i], marker = mark)
+            if is_print_value
+                @printf("  %8s: [", labs[i])
+                println(join([@sprintf(" %8.2e", eff) for eff in efficiency], ","), "]")
+            end
+        end
+
+        ## Add the "ideal" reference plot
+        if (is_plot_ideal)
+            x_ref, y_ref = dats[end]  # choose the last dataset as the reference
+            ideal_y = fill(1.0, length(x_ref))
             plot!(plt, x_ref, ideal_y; label = "Ideal", linestyle = :dash, color = :red)
         end
     end
